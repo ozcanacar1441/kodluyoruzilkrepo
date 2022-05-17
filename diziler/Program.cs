89e053a6faa44a8e6fed5d4cc1316247907e0114
Özcan <105226893+ozcanacar1441@users.noreply.github.com>
2022-05-17 17:17:18 +0300

    Add files via upload

---
+++ diziler/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace diziler // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string[] renkler=new string[5];
+           string[] hayvanlar={"kedi","köpek","kuş","maymun"};
+           int[] dizi;
+           dizi=new int[5];
+           renkler[0]="Mavi";
+           dizi[3]=10;
+           Console.WriteLine(hayvanlar[1]);
+           Console.WriteLine(dizi[3]);
+           Console.WriteLine(renkler[0]);
+            //klavyeden girilen n sayının ortalamasını hesaplayan program
+           Console.Write("Lütfen dizi eleman sayısını giriniz:");
+           int diziUzunlugu=int.Parse(Console.ReadLine());
+           int[] sayiDizisi=new int[diziUzunlugu];
+
+                for(int i=0;i<diziUzunlugu;i++)
+                {
+                    Console.Write("Lürfen {0}. sayıyı giriniz", i+1);
+                    sayiDizisi[i]=int.Parse(Console.ReadLine());
+                }
+                int toplam=0;
+                foreach(var sayi in sayiDizisi)
+                {
+                    toplam+=sayi;
+                }
+                Console.WriteLine("Ortalama = "+toplam/diziUzunlugu);
+        }
+    }
+}
