6399b91e24d6438219c21e07f7b106030be8a064
Özcan <105226893+ozcanacar1441@users.noreply.github.com>
2022-05-23 18:44:56 +0300

    Add files via upload

---
+++ struct_karvrami/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace struct_kavrami // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dikdörtgen dikdörtgen= new Dikdörtgen();
+            dikdörtgen.KisaKenar=3;
+            dikdörtgen.UzunKenar=4;
+            Console.WriteLine("Class Alan hesabı:{0}",dikdörtgen.AlanHesapla());
+
+            Dikdörtgen_Struct dikdörtgen_Struct=new Dikdörtgen_Struct(3,4);
+            
+             Console.WriteLine("Class Alan hesabı:{0}",dikdörtgen_Struct.AlanHesapla());
+
+
+        }
+    }
+    class Dikdörtgen
+    {
+        public int KisaKenar;
+        public int UzunKenar;
+        public Dikdörtgen(){
+            KisaKenar=3;
+            UzunKenar=4;
+        }
+
+        public long AlanHesapla(){
+            return this.KisaKenar*this.UzunKenar;
+        }
+    }
+    struct Dikdörtgen_Struct{
+       public int KisaKenar;
+       public int UzunKenar;
+       public Dikdörtgen_Struct(int kisaKenar,int uzunKenar){
+            KisaKenar=kisaKenar;
+            UzunKenar=uzunKenar;
+        }
+       public long AlanHesapla(){
+            return this.KisaKenar*this.UzunKenar;
+        }
+    }
+}
